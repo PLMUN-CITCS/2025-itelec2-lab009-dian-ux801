514af20b539235ff321a7a24dba7bb21927b455a
dian-ux801 <principeprincessdianne_bsa@plmun.edu.ph>
2025-02-16 14:46:21 +0800

    Submitting Python Week 04 - Laboratory # 09

---
+++ if_else_statement.py
@@ -0,0 +1,13 @@
+# Program to check if a number is Even or Odd
+
+try:
+    user_input = input("Enter a number: ")
+    number = int(user_input)
+
+    if number % 2 == 0:
+        print("The number", number, "is Even.")
+    else:
+        print("The number", number, "is Odd.")
+
+except ValueError:
+    print("Invalid input. Please enter an integer.")
